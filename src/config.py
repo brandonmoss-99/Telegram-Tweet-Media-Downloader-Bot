78c049325a27deb27ce81a3891a01958284e3090
Brandon Moss <88378432+brandonmoss-99@users.noreply.github.com>
2022-12-27 20:27:36 +0000

    Make changes for extra tMsgText tests
    Separated setting configuration log level and bot token to their own functions, so each can be tested
    
    Moved checking if allowed to reply to message to its own function, so it can be tested individually
    
    Wrote additional tests, to check for validURLs, invalidURLs, the getInfo logic and the checking of a message's sender id to the allowed to reply list

--- src/config.py
+++ src/config.py
@@ -8,27 +8,28 @@ class Config:
             case str(x):
                 match x.lower():
                     case "debug":
-                        self.logLevel = logging.DEBUG
+                        self.setLogLevel(logging.DEBUG)
                         self.printLogLevel(x)
                     case "info":
-                        self.logLevel = logging.INFO
+                        self.setLogLevel(logging.INFO)
                         self.printLogLevel(x)
                     case "warn" | "warning":
-                        self.logLevel = logging.WARN
+                        self.setLogLevel(logging.WARN)
                         self.printLogLevel(x)
                     case "error" | "err":
-                        self.logLevel = logging.ERROR
+                        self.setLogLevel(logging.ERROR)
                         self.printLogLevel(x)
                     case "critical" | "crit":
-                        self.logLevel = logging.CRITICAL
+                        self.setLogLevel(logging.CRITICAL)
                         self.printLogLevel(x)
                     case _:
-                        self.logLevel = logging.INFO
+                        self.setLogLevel(logging.INFO)
                         self.printLogLevel("info")
             case _:
                 # The default log level if couldn't find/parse the env variable
-                self.logLevel = logging.INFO
+                self.setLogLevel(logging.INFO)
                 self.printLogLevel("info")
+    
     
     def printLogLevel(self, level: str) -> None:
         print(f"Using logger with min log level of {level.upper()}")
@@ -44,16 +45,28 @@ class Config:
         # Filter the ID list to only those which are digits, then convert those into ints
         logging.info("Parsing ALLOWED_IDS list")
         if rawAllowedIds != None:
-            self.allowedIds: list = list(map(lambda x: int(x), list(filter(lambda x: str.isdigit(x), rawAllowedIds.split(',')))))
+            self.setAllowedIds(list(map(lambda x: int(x), list(filter(lambda x: str.isdigit(x), rawAllowedIds.split(','))))))
             logging.debug("ALLOWED_IDS list parse successful")
         else:
-            self.allowedIds: list = []
+            self.setAllowedIds([])
             logging.warn("ALLOWED_IDS list parse couldn't find any IDS, not allowing anyone to talk to the bot for security")
         
         logging.info("Parsing Telegram token")
         if type(rawToken) == str:
-            self.tToken: str = str(rawToken)
+            self.setToken(str(rawToken))
             logging.debug("Token parse successful")
         else:
             logging.error("Token parse found empty token, Telegram will reject this bot")
-            self.tToken: str = ""
+            self.setToken("")
+    
+
+    def setAllowedIds(self, allowed: list) -> None:
+        self.allowedIds: list = allowed
+    
+
+    def setToken(self, t: str) -> None:
+        self.tToken: str = t
+
+
+    def setLogLevel(self, l: int) -> None:
+        self.logLevel: int = l
