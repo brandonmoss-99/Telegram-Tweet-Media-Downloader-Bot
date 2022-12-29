6291bcff2c4ea531b280de34cf23c89207e2838b
Brandon Moss <88378432+brandonmoss-99@users.noreply.github.com>
2022-12-29 10:37:19 +0000

    Add support for downloading from Caption
    Removed the tMsgHandler, as it had no real beneficial purpose
    
    Added check for if message has text content, and to use the caption text if not

--- src/tMsgText.py
+++ src/tMsgText.py
@@ -9,12 +9,14 @@ class tMsgText:
         self.config = config
         self.urlRegex: str = r'http[s]?://(?:[a-zA-Z]|[0-9]|[^?\s])+'
 
+
     def getInfo(self):
         # extract always included message data
         self.message_id = self.message['message_id']
         self.date = self.message['date']
         self.chat = self.message['chat']
         self.isfrom = self.message['from']
+
 
     def checkCanReply(self, id) -> bool:
         logging.debug(f"Checking if allowed to reply to userID: {id}")
@@ -26,33 +28,44 @@ class tMsgText:
                 logging.info(f"Not allowed to process things from userID: {id}")
                 return False
 
+
     def process(self):
         # Check for userID who sent the msg. Only do stuff if they're allowed to send stuff
         if self.checkCanReply(self.isfrom['id']) == False:
             logging.info(f"Sending not on allow list message for userID: {self.isfrom['id']}")
             self.tMsgSender.sendRequest(["sendMessage", "chat_id", self.chat['id'], "text", "Sorry, you're not on my allow list! Zzzz...", "disable_web_page_preview", True, "disable_notification", True])
             return
-            
-        logging.info(f"Allowed to reply to userID: {self.isfrom['id']}")
+        
+        # If text isn't in the message, the user most likely sent media with text in the caption instead
+        if 'text' not in self.message and 'caption' in self.message:
+            self.doDownloading(self.message['caption'])
+            return
+        
         match self.message['text']:
             case "/start":
                 # what to say when a new person talks to the bot
                 logging.info(f"Received /start message from userID {self.isfrom['id']}, replying with info text")
                 self.tMsgSender.sendRequest(["sendMessage", "chat_id", self.chat['id'], "text", "Hi! Please send a URL to get started!", "disable_web_page_preview", True, "disable_notification", True])
             case msg:
-                urls: list[str] = self.parseRegex(msg)
-                if len(urls) > 0:
-                    self.downloadContent(urls)
-                else:
-                    logging.info(f"Replying couldn't find URL to userID {self.isfrom['id']}")
-                    self.reply([False, "Couldn't find a valid URL to use"])
-    
+                self.doDownloading(msg)
+        
+
+    def doDownloading(self, text):
+        urls: list[str] = self.parseRegex(text)
+        if len(urls) > 0:
+            self.downloadContent(urls)
+        else:
+            logging.info(f"Replying couldn't find URL to userID {self.isfrom['id']}")
+            self.reply([False, "Couldn't find a valid URL to use"])
+
+
     def parseRegex(self, toParse: str) -> list[str]:
         logging.debug(f"Parsing text against regex")
         urls: list[str] = re.findall(self.urlRegex, toParse)
         logging.debug(f"Found {len(urls)} matches")
         return urls
     
+
     def downloadContent(self, urls: list[str]):
         for url in urls:
             # Download the contents of the URL, and send reply
@@ -65,6 +78,7 @@ class tMsgText:
                 case _: 
                     logging.info(f"Replying failed download for {url} to userID {self.isfrom['id']}")
                     self.reply([False, f"Encountered an error whilst downloading content for {url}"])
+
 
     def reply(self, data):
         # if succeeded in fetching data for valid station code, reply with info
