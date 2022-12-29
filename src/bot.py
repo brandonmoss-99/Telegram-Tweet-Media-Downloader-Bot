560abbe048e2fcd4af0f223346173ac4fa81fdbe
Brandon Moss <88378432+brandonmoss-99@users.noreply.github.com>
2022-12-29 14:55:13 +0000

    Change bot.py if-else to match

--- src/bot.py
+++ src/bot.py
@@ -26,12 +26,13 @@ if __name__ == '__main__':
         # connect to Telegram API with their getMe test method for checking API works
         testResponse: requests.Response = requests.get(f"https://api.telegram.org/bot{config.tToken}/getMe")
         # set the token to be used if we get a 2xx response code back
-        if testResponse.ok:
-            bottoken: str = config.tToken
-            logging.info("Telegram API token OK")
-        else:
-            logging.error("Telegram API check failed")
-            getHelp()
+        match testResponse.ok:
+            case True:
+                bottoken: str = config.tToken
+                logging.info("Telegram API token OK")
+            case _:
+                logging.error("Telegram API check failed")
+                getHelp()
     except Exception as ex:
         logging.error("Telegram API token verification failed", exc_info=ex)
         getHelp()
@@ -50,18 +51,17 @@ if __name__ == '__main__':
         logging.info("Sending off to wait for new data")
         response: messageInfo = tMsgFetcher.fetchMessages(msgOffset)
         logging.info("Received new Telegram data")
-        if response.tResponseOk:
-            logging.info("Telegram response was OK")
-            for msg in response.tResult:
-                logging.info("Handling message(s)")
-                tMsgText(msg['message'], tMsgSender, config).process()
-                msgOffset = msg['update_id'] + 1  # Update msg offset
-                logging.info(f"Message offset updated to {msgOffset}")
-        else:
-            logging.warn(f"Telegram response indicated error! {response.errCode} - {response.errDesc}")
-            # failed to fetch new messages, wait for random number of seconds then try again
-            # (may reduce strain on telegram servers when requests are randomly distributed if
-            # they go down, instead of happening at fixed rate along with many other bots etc)
-            sleepTime: int = random.randint(pollTimeout, pollTimeout*2)
-            logging.info(f"Sleeping for {sleepTime} seconds")
-            time.sleep(sleepTime)
+        match response.tResponseOk:
+            case True:
+                logging.info("Telegram response was OK")
+                for msg in response.tResult:
+                    logging.info("Handling message(s)")
+                    tMsgText(msg['message'], tMsgSender, config).process()
+                    msgOffset = msg['update_id'] + 1  # Update msg offset
+                    logging.info(f"Message offset updated to {msgOffset}")
+            case _:
+                logging.warn(f"Telegram response indicated error! {response.errCode} - {response.errDesc}")
+                # failed to fetch new messages, wait for random number of seconds then try again
+                sleepTime: int = random.randint(pollTimeout, pollTimeout*2)
+                logging.info(f"Sleeping for {sleepTime} seconds")
+                time.sleep(sleepTime)
