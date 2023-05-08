8c7bec0e8f95e1548058c6ef0494e4c3d02f44b9
lancer-wang <m15565236782@163.com>
2023-05-08 09:55:17 +0800

    test

--- src/tMsgSender.py
+++ src/tMsgSender.py
@@ -43,19 +43,49 @@ class tMsgSender:
     def sendSilentMessage(self, text: str, chat_id: str) -> recievedData:
         return self.sendRequest(["sendMessage", "chat_id", chat_id, "text", text, "disable_web_page_preview", True, "disable_notification", True])
 
-    def sendPhoto(self, photo_path: str, chat_id: str) -> recievedData:
-        return self.sendRequest(["sendPhoto", "chat_id", chat_id], files={"photo": open(photo_path, "rb")})
+    # def sendPhoto(self, photo_path: str, chat_id: str) -> recievedData:
+    #     return self.sendRequest(["sendPhoto", "chat_id", chat_id], files={"photo": open(photo_path, "rb")})
 
-    def sendVideo(self, video_path: str, chat_id: str) -> recievedData:
-        return self.sendRequest(["sendVideo", "chat_id", chat_id], files={"video": open(video_path, "rb")})
+    # def sendVideo(self, video_path: str, chat_id: str) -> recievedData:
+    #     return self.sendRequest(["sendVideo", "chat_id", chat_id], files={"video": open(video_path, "rb")})
 
-    def sendMultiplePhotos(self, photo_paths, chat_id: str) -> recievedData:
-        files = [('photo' + str(i), open(photo_path, 'rb')) for i, photo_path in enumerate(photo_paths)]
-        logging.info("#####################")
-        logging.info(files)
-        params = ['sendMediaGroup', 'chat_id', chat_id]
+    def sendMultiplePhotos(self, chat_id: str, photo_files: list, caption: str=None):
+        """
+        Send multiple photos in a single message
+        
+        Arguments:
+        chat_id: str -- The ID of the chat where message should be sent
+        photo_files: list -- List of file objects representing photos to be sent.
+        caption: str or None -- Caption for the photos. If None, no caption will be sent.
+        """
+        url = f"{self.tAPIUrl}/sendMediaGroup"
+        media = []
+        
+        for photo_file in photo_files:
+            media.append({
+                "type": "photo",
+                "media": photo_file
+            })
+        
+        payload = {
+            "chat_id": chat_id,
+            "media": media
+        }
+        
+        if caption is not None:
+            payload["caption"] = caption
+        
+        response = requests.post(url, json=payload)
+        logging.info(response)
+        if response.status_code != 200:
+            raise Exception(f"Failed to send message. Status code: {response.status_code}")
+    # def sendMultiplePhotos(self, photo_paths, chat_id: str) -> recievedData:
+    #     files = [('photo' + str(i), open(photo_path, 'rb')) for i, photo_path in enumerate(photo_paths)]
+    #     logging.info("#####################")
+    #     logging.info(files)
+    #     params = ['sendMediaGroup', 'chat_id', chat_id]
 
-        return self.sendRequest(params, files)
+    #     return self.sendRequest(params, files)
 
 
     def sendRequest(self, msgParams: list, files=None) -> recievedData:
